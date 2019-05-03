553a1d22a94e5c951aed8c410a91f312b63e7f8d
Gakuto Yamada <gakuto@gakuto-no-imac.lan>
2019-05-03 22:20:29 +0900

    add setup shell

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dotfiles
+
+setting up
+
+```
+bash -c "$(curl -L https://raw.githubusercontent.com/gactocat/dotfiles/master/setup.sh)"
+```
+
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DOTPATH=$HOME/dotfiles
+
+if [ ! -d "$DOTPATH" ]; then
+    git clone https://github.com/gactocat/dotfiles.git "$DOTPATH"
+else
+    echo "$DOTPATH already downloaded. Updating..."
+    cd "$DOTPATH"
+    git stash
+    git checkout master
+    git pull origin master
+    echo
+fi
+
+cd "$DOTPATH"
+
