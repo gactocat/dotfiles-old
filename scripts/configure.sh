fab0f77a7452802f88b95bfe90ca1868d08e03f9
gactocat <gactocat@gmail.com>
2019-05-04 19:07:23 +0900

    expand configure

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
 if ! command -v defaults > /dev/null 2>&1; then
     echo "\`defaults\` not found. Nothing to do."
     exit 0
 fi
 
+echo "Configuring..."
 defaults write -g AppleLanguages '( "en-JP", "ja-JP")'
+
+echo "Configuring Finder..."
 defaults write com.apple.finder AppleShowAllFiles -boolean true # 隠しファイルを表示する
+killall Finder
+
+echo "Configuring Dock..."
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock magnification -int 1
+defaults write com.apple.dock mru-spaces -bool false
+killall Dock
+
+echo "Configuring Trackpad..."
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerPinchGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFiveFingerPinchGesture -int 2
+defaults write com.apple.dock showMissionControlGestureEnabled -bool true
+defaults write com.apple.dock showAppExposeGestureEnabled -bool true
+defaults write com.apple.dock showDesktopGestureEnabled -bool true
+defaults write com.apple.dock showLaunchpadGestureEnabled -bool true
